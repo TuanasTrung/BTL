0541fde0a114bae31a4ff3580f94b436d58b68da
Chinh <Chinhvu1031@gmail.com>
2022-04-11 03:25:47 +0700

    no message

--- app/src/main/java/com/example/btl/LoginActivity.java
+++ app/src/main/java/com/example/btl/LoginActivity.java
@@ -1,4 +1,20 @@
 package com.example.btl;
 
-public class LoginActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+public class LoginActivity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        Textview btn=findViewById(R.id.textViewSignUp);
+        btn.setOnClickListener(new View.OnclickListener() {
+            aoverride
+            public void onclick(View v) {
+                startActivity(new Intent( packageContert: LoginActivity.this, RegisterActivity.class));
+            };
+
+    }
 }
